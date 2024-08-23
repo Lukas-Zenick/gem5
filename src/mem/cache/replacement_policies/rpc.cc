4e0ad57518bcb42629556f528ae8787a448b8ec6
Lukas Zenick <lukas@cs.utexas.edu>
2024-08-23 13:32:07 -0500

    gem5config

---
+++ src/mem/cache/replacement_policies/rpc.cc
@@ -0,0 +1,227 @@
+#include "mem/cache/replacement_policies/rpc.hh"
+#include "base/logging.hh" // For fatal_if
+#include "params/RPC.hh"
+#include "debug/MockingjayDebug.hh"
+
+namespace gem5
+{
+namespace replacement_policy
+{
+
+uint64_t getAccessType(const PacketPtr &pkt) {
+    if (pkt->isRead() && !pkt->isWriteback()) {
+        return 0; // LOAD
+    } else if (pkt->cmd == MemCmd::WriteReq) { // Assuming WriteReq as RFO for simplification
+        return 1; // RFO?
+    } else if (pkt->isWriteback()) {
+        return 2; // WRITEBACK
+    } else if (pkt->req->isPrefetch()) {
+        return 3; // PREFETCH
+    } else {
+        return 0; // Default case, adjust based on your requirements
+    }
+}
+
+RPC::RPC(const RPCParams &params)
+    : Base(params),  _num_etr_bits(params.num_etr_bits) {
+    sampled_cache = std::make_unique<rpc::SampledCache>(params.num_sampled_sets, params.num_cache_sets, params.cache_block_size, params.timer_size, params.num_cpus, params.num_internal_sampled_sets);
+    predictor = std::make_unique<rpc::ReuseDistPredictor>(params.num_pred_entries, params.num_pred_bits, params.num_clock_bits, params.num_cpus);
+    age_ctr.resize(params.num_cache_sets, 0);
+    _log2_block_size = (int) std::log2(params.cache_block_size);
+    _log2_num_cache_sets = (int) std::log2(params.num_cache_sets);
+    _num_clock_bits = params.num_clock_bits;
+
+    DPRINTF(MockingjayDebug, "Cache Initialization ---- Number of Cache Sets: %d, Cache Block Size: %d, Number of Cache Ways: %d\n", params.num_cache_sets, params.cache_block_size, params.num_cache_ways);
+    DPRINTF(MockingjayDebug, "History Sampler Initialization ---- Number of Sample Sets: %d, Timer Size: %d\n", params.num_sampled_sets, params.timer_size);
+    DPRINTF(MockingjayDebug, "Predictor Initialization ---- Number of Predictor Entries: %d, Counter of Predictors: %d\n", params.num_pred_entries, params.num_pred_bits);
+    DPRINTF(MockingjayDebug, "CPU Core Initialization ---- Number of Cores: %d\n", params.num_cpus);
+}
+
+void
+RPC::invalidate(const std::shared_ptr<ReplacementData>& replacement_data) {
+    std::shared_ptr<RPCReplData> casted_replacement_data = std::static_pointer_cast<RPCReplData>(replacement_data);
+    casted_replacement_data->valid = false;
+    casted_replacement_data->etr = 0;
+}
+
+void
+RPC::touch(const std::shared_ptr<ReplacementData>& replacement_data, const PacketPtr pkt, const ReplacementCandidates& candidates) {
+    std::shared_ptr<RPCReplData> casted_replacement_data =
+        std::static_pointer_cast<RPCReplData>(replacement_data);
+
+    // TODO: Which requests should we monitor?
+    if (!pkt->isRequest() || !pkt->req->hasPC() || !pkt->req->hasContextId()) {
+        //TODO: Change to hasVaddr?
+        return;
+    }
+
+    // DPRINTF(MockingjayDebug, "Cache hit ---- Packet type having PC: %s\n", pkt->cmdString());
+
+    // Warning: This is not aligned with indexing policy if it use interleave set indexing technique
+    int set = (pkt->getAddr() >> _log2_block_size) & ((1 << _log2_num_cache_sets) - 1);
+
+    DPRINTF(MockingjayDebug, "Cache hit ---- Request Address: 0x%.8x, Set Index: %d\n", pkt->getAddr(), set);
+
+    // Warning: Timestamp is 8-bit integer in this design
+    uint8_t curr_timestamp = 0;
+    uint8_t last_timestamp = 0;
+    uint16_t last_PC = 0;
+    bool evict = false;
+    bool sample_hit = false;
+
+    // Cache hit
+    // Sampled cache
+    //  1. If sampled cache hit, predictor will train with signature in the sampled cache for new reuse distance
+    //  2. If sampled cache miss and sampled cache no eviction, no training needed
+    //  3. If sampled cache miss and sampled cache eviction, the eviction line should be detrained as scan line
+
+    uint64_t access_type = getAccessType(pkt);
+
+    if (sampled_cache->sample(pkt->getAddr(), access_type, &curr_timestamp, set, &last_PC, &last_timestamp, true, &evict, &sample_hit, pkt->req->contextId(), predictor->getInfRd())) {
+        predictor->train(last_PC, sample_hit, curr_timestamp, last_timestamp, evict);
+        DPRINTF(MockingjayDebug, "Cache hit ---- Sampler, Last timestamp: %d, Current timestamp: %d, Last PC: 0x%.8x\n", last_timestamp, curr_timestamp, last_PC);
+    }
+
+    // If aging clock reaches maximum point, then all cache lines should be aged.
+    uint64_t aging_max = (1 << _num_clock_bits) - 1;
+    if (age_ctr[set] != aging_max) {
+        age_ctr[set] += 1;
+    } else {
+        age_ctr[set] = 0;
+
+        for (const auto &candidate : candidates) {
+            std::shared_ptr<RPCReplData> candidate_repl_data =
+                std::static_pointer_cast<RPCReplData>(
+                    candidate->replacementData);
+            candidate_repl_data->aging();
+        }
+    }
+    // casted_replacement_data->etr = predictor->predict(pkt->req->getPC(), true, pkt->req->contextId(), casted_replacement_data->abs_max_etr);
+    casted_replacement_data->etr = predictor->predict(pkt->getAddr(), access_type, true, pkt->req->contextId(), casted_replacement_data->abs_max_etr);
+    DPRINTF(MockingjayDebug, "Cache hit ---- ETR update: %d, INF_ETR: %d\n", casted_replacement_data->etr, casted_replacement_data->abs_max_etr);
+}
+
+void
+RPC::reset(const std::shared_ptr<ReplacementData>& replacement_data, const PacketPtr pkt, const ReplacementCandidates& candidates) {
+    std::shared_ptr<RPCReplData> casted_replacement_data =
+        std::static_pointer_cast<RPCReplData>(replacement_data);
+
+    uint64_t access_type = getAccessType(pkt);
+
+    // TODO: Which requests should we monitor?
+    if (!pkt->isResponse() || !pkt->req->hasVaddr() || !pkt->req->hasContextId()) {
+        return;
+    }
+
+    // ETR for replacement_data should be the maximum absolute value in the whole set
+    // if (predictor->bypass(pkt->req->getPC(), casted_replacement_data->etr, false, pkt->req->contextId(), casted_replacement_data->valid)) {
+    if(predictor->bypass(pkt->getAddr(), getAccessType(pkt), casted_replacement_data->abs_max_etr, false, pkt->req->contextId(), casted_replacement_data->valid)) {
+        // DPRINTF(MockingjayDebug, "Cache miss ---- Bypass cache: PC: 0x%.8x\n", pkt->req->getPC());
+        return;
+    }
+
+    // DPRINTF(MockingjayDebug, "Cache miss ---- Packet type having PC: %s\n", pkt->cmdString());
+
+    // Warning: This is not aligned with indexing policy if it use interleave set indexing technique
+    int set = (pkt->getAddr() >> _log2_block_size) & ((1 << _log2_num_cache_sets) - 1);
+
+    // DPRINTF(MockingjayDebug, "Cache miss ---- Request Address: 0x%.8x, Set Index: %d, PC: 0x%.8x\n", pkt->getAddr(), set, pkt->req->getPC());
+
+    // Warning: Timestamp is 8-bit integer in this design
+    uint8_t curr_timestamp = 0;
+    uint8_t last_timestamp = 0;
+    uint16_t last_PC = 0;
+    bool evict = false;
+    bool sample_hit = false;
+
+    // Cache miss
+    // Sampled cache
+    //  1. If sampled cache hit, predictor will train with signature in the sampled cache for new reuse distance
+    //  2. If sampled cache miss and sampled cache no eviction, no training needed
+    //  3. If sampled cache miss and sampled cache eviction, the eviction line should be detrained as scan line
+    if (sampled_cache->sample(pkt->getAddr(), access_type, &curr_timestamp, set, &last_PC, &last_timestamp, false, &evict, &sample_hit, pkt->req->contextId(), predictor->getInfRd())) {
+        predictor->train(last_PC, sample_hit, curr_timestamp, last_timestamp, evict);
+        DPRINTF(MockingjayDebug, "Cache miss ---- Sampler, Last timestamp: %d, Current timestamp: %d, Last PC: 0x%.8x\n", last_timestamp, curr_timestamp, last_PC);
+    }
+
+    // If aging clock reaches maximum point, then all cache lines should be aged.
+    uint64_t aging_max = (1 << _num_clock_bits) - 1;
+    if (age_ctr[set] != aging_max) {
+        age_ctr[set] += 1;
+    } else {
+        age_ctr[set] = 0;
+
+        for (const auto &candidate : candidates) {
+            std::shared_ptr<RPCReplData> candidate_repl_data =
+                std::static_pointer_cast<RPCReplData>(
+                    candidate->replacementData);
+            candidate_repl_data->aging();
+        }
+    }
+
+    // replacement status update
+    // casted_replacement_data->etr = predictor->predict(pkt->getAddr(), false, pkt->req->contextId(), casted_replacement_data->abs_max_etr);
+    casted_replacement_data->etr = predictor->predict(pkt->getAddr(), access_type, false, pkt->req->contextId(), casted_replacement_data->abs_max_etr);
+    DPRINTF(MockingjayDebug, "Cache miss ---- ETR update: %d, INF_ETR: %d\n", casted_replacement_data->etr, casted_replacement_data->abs_max_etr);
+    casted_replacement_data->valid = true;
+}
+
+ReplaceableEntry*
+RPC::getVictim(const ReplacementCandidates& candidates) const {
+    assert(candidates.size() > 0);
+
+    // Use first candidate as dummy victim
+    ReplaceableEntry* victim = candidates[0];
+
+    // Store victim->etr in a variable to improve code readability
+    int victim_etr = std::static_pointer_cast<RPCReplData>(victim->replacementData)->etr;
+    int abs_victim_etr = std::abs(victim_etr);
+
+    // Visit all candidates to find victim
+    for (const auto& candidate : candidates) {
+        std::shared_ptr<RPCReplData> candidate_repl_data =
+            std::static_pointer_cast<RPCReplData>(
+                candidate->replacementData);
+
+        // Stop searching for victims if an invalid entry is found
+        if (!candidate_repl_data->valid) {
+            return candidate;
+        }
+
+        // Update victim entry if necessary
+        // Choose the one with maximum reuse distance (ETR allow negative value to avoid cache line s)
+        int candidate_etr = candidate_repl_data->etr;
+        int abs_candidate_etr = std::abs(candidate_etr);
+        if (abs_candidate_etr > abs_victim_etr || ((abs_candidate_etr == abs_victim_etr) && (candidate_etr < 0))) {
+            victim = candidate;
+            victim_etr = candidate_etr;
+            abs_victim_etr = abs_candidate_etr;
+        }
+    }
+
+    return victim;
+}
+
+std::shared_ptr<ReplacementData>
+RPC::instantiateEntry() {
+   return std::shared_ptr<ReplacementData>(new RPCReplData(_num_etr_bits));
+}
+
+void RPC::reset(const std::shared_ptr<ReplacementData>& replacement_data) const {
+    panic("Cant train Mockingjay's predictor without access information.");
+}
+
+void RPC::touch(const std::shared_ptr<ReplacementData>& replacement_data) const {
+    panic("Cant train Mockingjay's predictor without access information.");
+}
+
+void RPC::reset(const std::shared_ptr<ReplacementData>& replacement_data, const PacketPtr pkt) {
+    panic("Cant train Mockingjay's predictor without all cache blocks reference. RESET DOES NOT HAVE THE REPLACEMENT CANDIDATES.");
+}
+
+void RPC::touch(const std::shared_ptr<ReplacementData>& replacement_data, const PacketPtr pkt) {
+    panic("Cant train Mockingjay's predictor without all cache blocks reference. TOUCH DOES NOT HAVE THE REPLACEMENT CANDIDATES.");
+}
+
+} // namespace replacement_policy
+} // namespace gem5
---
+++ src/mem/cache/tags/rpc_sampler.hh
@@ -0,0 +1,264 @@
+#include <cassert>
+#include <cstdint>
+#include <random>
+#include <set>
+#include "base/trace.hh"
+#include "debug/MockingjayDebug.hh"
+#include "base/sat_counter.hh"
+
+namespace gem5 {
+namespace rpc {
+
+static constexpr uint32_t NUM_WAY_CACHE_SET = 5;
+static constexpr uint32_t HASHED_PC_LEN = 11;
+static constexpr uint64_t HASHED_PC_MASK = ((1 << HASHED_PC_LEN) - 1);
+static constexpr uint32_t TIMESTAMP_LEN = 8;
+static constexpr uint64_t TIMESTAMP_LEN_MASK = ((1 << TIMESTAMP_LEN) - 1);
+static constexpr uint32_t ADDRESS_TAG_LEN = 10;
+static constexpr uint64_t ADDRESS_TAG_MASK = ((1 << ADDRESS_TAG_LEN) - 1);
+
+static int time_elapsed(int global, int local) {
+    if (global >= local) {
+        return global - local;
+    }
+    global = global + (1 << TIMESTAMP_LEN);
+    return global - local;
+}
+
+/**
+ * Entry: 2-byte Address, 2-byte PC, 1-byte timestamp
+ *
+ * Sample the whold cache with 64 random sets. The prediction need 8x history of set associativity
+ *
+ * Set-associative cache: Each set has 128 entries (??? Which structure of this sampled cache)
+ *
+ * LRU replacement policy
+ */
+class SampledCache
+{
+
+  protected:
+    struct CacheLine
+    {
+
+      bool valid;
+
+      uint8_t lru;
+
+      uint64_t _address;
+
+      uint64_t _pc;
+
+      uint64_t _timestamp;
+
+      uint16_t getAddress() {
+        gem5_assert(_address < (1 << ADDRESS_TAG_LEN), "Address bits are wrong, address: 0x%.8x", _address);
+        return _address;
+      }
+
+      uint16_t getPC() {
+        // gem5_assert(_pc < (1 << HASHED_PC_LEN), "PC bits are wrong");
+        return _pc;
+      }
+
+      uint8_t getTimestamp() {
+        gem5_assert(_timestamp < (1 << TIMESTAMP_LEN), "timestamp bits are wrong");
+        return _timestamp;
+      }
+
+      void setPC(uint16_t PC) {
+        // gem5_assert(_pc < (1 << HASHED_PC_LEN), "PC bits are wrong");
+        _pc = PC;
+      }
+
+      void setTimestamp(uint8_t timestamp) {
+        gem5_assert(_timestamp < (1 << TIMESTAMP_LEN), "timestamp bits are wrong");
+        _timestamp = timestamp;
+      }
+
+      void setAddrTag(uint16_t addrtag) {
+        gem5_assert(addrtag < (1 << ADDRESS_TAG_LEN), "Address bits are wrong");
+        _address = addrtag;
+      }
+
+      CacheLine() : valid(false), lru(0), _address(0), _pc(0), _timestamp(0) {};
+
+    };
+
+    struct CacheSet
+    {
+      struct CacheLine ways[NUM_WAY_CACHE_SET];
+
+      bool insert(uint16_t addr_tag, uint16_t PC, uint8_t timestamp, uint8_t *evict_timestamp, uint16_t *evict_signature, uint64_t inf_rd) {
+        // Assume address and PC has been translated
+        bool insert_with_evict = false;
+        int evict_way = -1;
+        int evict_lru = NUM_WAY_CACHE_SET;
+
+        for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+          if (!ways[i].valid) {
+            evict_way = i;
+            evict_lru = 0;
+          }
+        }
+
+        // Only one way will be evicted
+        if (evict_way < 0) {
+          for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+            if (time_elapsed(timestamp, ways[i].getTimestamp()) > inf_rd) {
+              evict_way = i;
+              evict_lru = ways[i].lru;
+              insert_with_evict = true;
+            }
+          }
+        }
+
+        if (evict_way < 0) {
+          for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+            if (ways[i].valid && ways[i].lru < evict_lru) {
+              evict_lru = ways[i].lru;
+              evict_way = i;
+            } else if (ways[i].valid && ways[i].lru == evict_lru) {
+              panic("LRU for sampled cache should not have the same value: Evict way: %d, Evict LRU value: %d, LRU: %d %d %d %d %d",
+                    evict_way, evict_lru, ways[0].lru, ways[1].lru, ways[2].lru, ways[3].lru, ways[4].lru);
+            }
+          }
+          gem5_assert(evict_way >= 0, "There should be one cache line evicted.");
+          insert_with_evict = true;
+        }
+
+        *evict_signature = ways[evict_way].getPC();
+        *evict_timestamp = ways[evict_way].getTimestamp();
+        for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+          if (ways[i].valid && ways[i].lru > evict_lru) {
+            gem5_assert(ways[i].lru > 0, "LRU bit cannot be negative");
+            ways[i].lru -= 1;
+          }
+        }
+        ways[evict_way].lru = NUM_WAY_CACHE_SET - 1;
+        ways[evict_way].setAddrTag(addr_tag);
+        ways[evict_way].setPC(PC);
+        ways[evict_way].setTimestamp(timestamp);
+        ways[evict_way].valid = true;
+
+        return insert_with_evict;
+      }
+
+      bool access(uint16_t addr_tag, uint16_t PC, uint8_t timestamp, uint16_t *last_PC, uint8_t *last_timestamp) {
+
+        for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+          if (ways[i].valid && addr_tag == ways[i].getAddress()) {
+            *last_PC = ways[i].getPC();
+            *last_timestamp = ways[i].getTimestamp();
+
+            ways[i].setPC(PC);
+            ways[i].setTimestamp(timestamp);
+
+            for (int j = 0; j < NUM_WAY_CACHE_SET; j++) {
+              if (ways[j].lru > ways[i].lru) {
+                ways[j].lru -= 1;
+              }
+            }
+            ways[i].lru = NUM_WAY_CACHE_SET - 1;
+            return true;
+          }
+        }
+        return false;
+      }
+
+      bool invalidate(uint16_t addr_tag) {
+        for (int i = 0; i < NUM_WAY_CACHE_SET; i++) {
+          if (addr_tag == ways[i].getAddress()) {
+            ways[i].valid = false;
+            for (int j = 0; j < NUM_WAY_CACHE_SET; j++) {
+              if (i != j && ways[j].lru > ways[i].lru) {
+                ways[j].lru -= 1;
+              }
+            }
+            return true;
+          }
+        }
+        return false;
+      }
+
+      CacheSet() {};
+    };
+
+  private:
+    struct CacheSet *sample_data;
+
+    uint64_t *set_timestamp_counter;
+
+    int _num_sampled_sets;
+
+    int _num_cache_sets;
+
+    int _cache_block_size;
+
+    int _log2_num_sampled_sets;
+
+    int _log2_cache_block_size;
+
+    int _timer_size;
+
+    int _num_cpus;
+
+    int _num_sampled_internal_sets;
+
+    int _log2_sampled_internal_sets;
+
+  public:
+
+    SampledCache(const int num_sampled_sets, const int num_cache_sets, const int cache_block_size, const int timer_size, const int num_cpus, const int num_sampled_internal_sets);
+
+    ~SampledCache();
+
+    bool sample(uint64_t addr, uint64_t access_type, uint8_t *curr_timestamp, int set, uint16_t *last_PC, uint8_t *last_timestamp, bool hit, bool *evict, bool *sample_hit, int core_id, uint64_t inf_rd);
+
+    uint64_t getCurrentTimestamp(int set);
+
+};
+
+/**
+ * 3-bit saturating counter, high-order bit determines whether it is cache-averse (0) or cache-friendly (1)
+ *
+ * 8K entries (2^11 => 13-bit hashed PC index)
+ */
+class ReuseDistPredictor
+{
+
+  private:
+
+    int *counters;
+
+    int num_entries;
+
+    int max_value = 0;
+
+    int bits_per_entry = 0;
+
+    int max_rd = 0;
+
+    int _granularity;
+
+    int _num_cpus;
+
+  public:
+
+    ReuseDistPredictor(const int num_entries, const int bits_per_entry, const int aging_clock_size, const int num_cpus);
+
+    ~ReuseDistPredictor();
+
+    void train(uint64_t last_PC, bool sampled_cache_hit, uint8_t curr_timestamp, uint8_t last_timestamp, bool evict);
+
+    uint16_t predict(uint64_t addr, uint64_t access_type, bool hit, int core_id, int etr_inf);
+
+    bool bypass(uint64_t addr, uint64_t access_type, uint8_t max_etr, bool hit, int core_id, bool cache_line_valid);
+
+    int getLog2NumEntries();
+
+    int getInfRd();
+};
+
+}
+}
